d43f73587e04aaa63f9a55a798417ed5ca5e9332
DELAGREEN <60788008+DELAGREEN@users.noreply.github.com>
2024-04-18 21:55:08 +0300

    add docker-file

--- main/eximg.py
+++ main/eximg.py
@@ -4,8 +4,11 @@ import cv2
 import numpy
 import icecream
 import pytesseract
+from pytesseract import Output
+import datetime
 
 
+start = datetime.datetime.now()
 
 ##def convert_pdf_to_tiff(input_pdf, output_folder):
 ##    convert_from_path(input_pdf, output_folder= output_folder)
@@ -27,27 +30,42 @@ def convert_pdf_to_tiff(path):
     pages1 = convert_from_path(path, dpi=300, 
                                #output_folder="D:\\REP\\projects\\drawing_text_recognition\\main", 
                                 poppler_path="D:\\REP\\projects\\drawing_text_recognition\\main\\poppler-24.02.0\\Library\\bin", fmt="png")
-    print(len(pages1))
-    print(type(pages1))
-    print(type(pages1[0]))
+    #print(len(pages1))
+    #print(type(pages1))
+    #print(type(pages1[0]))
     img = numpy.array(pages1[0])
-    print(type(img))
+    #print(type(img))
     #img=cv2.imread(pages1[0])
     #img_np = cv2.imdecode(img)
     #img1 = cv2.imread(img)
     #print(f"img type: {type(img1)}")
     img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    #print(f'img_shape0: {img_rgb.shape[0]}')
+    #print(f'img_shape1: {img_rgb.shape[1]}')
+    #print(f'img_shape2: {img_rgb.shape[2]}')
+    #print(f'img_shape3: {img_rgb.shape[3]}')
+    #print(f'img_full_shape:{img_rgb.shape}')
+    #shape = int((img_rgb.shape[0]/100)*20)
+    #print(f'shape: {shape}')
+    #crop_img = img_rgb[img_rgb.shape[0] - int((img_rgb.shape[0]/100)*20):img_rgb.shape[0] - int((img_rgb.shape[0]/100)*20), 
+    #                   img_rgb.shape[1] - int((img_rgb.shape[0]/100)*20):img_rgb.shape[1] - int((img_rgb.shape[0]/100)*5)]
+    
+    crop_img2 = img_rgb[int((img_rgb.shape[0]/100)*1):int((img_rgb.shape[0]/100)*80), 
+                       int((img_rgb.shape[0]/100)*65):img_rgb.shape[1]]             #[0] - шинира, [1] - высота
+    
+    resize = (int((img_rgb.shape[0]/100)*20), int((img_rgb.shape[1]/100)*20))
+    resize_img = cv2.resize(crop_img2, resize)
+    print(img_rgb.shape)
     #print(img_rgb)
     #print(pytesseract.image_to_string(img))
-    print(type(img_rgb))
-    print(pytesseract.image_to_string(img_rgb, "num"))
-    #print(pytesseract.pytesseract.image_to_string(image=img_rgb, lang="num"))
-
-    cv2.imshow('Result', img)
+    #print(type(img_rgb))
+    ##print(pytesseract.image_to_string(img_rgb, "num"))
+    print(pytesseract.pytesseract.image_to_string(image=img_rgb, lang="rus", output_type = Output.STRING))
+    ##print(pytesseract.image_to_data(img_rgb, 'num'))
+    #print(pytesseract.run_and_get_output(img_rgb, 'output', lang="num", config="hocr"))
+    cv2.imshow('Result', resize_img)
     cv2.waitKey(0) 
-   # for page in pages:
-   #
-   #      Image.open(page).save(f"{path.stem}.tif")
+   
 
 path = 'D:\\REP\\projects\\drawing_text_recognition\\pdf\\001сб.dwg.pdf'
 convert_pdf_to_tiff(path)
@@ -63,3 +81,7 @@ convert_pdf_to_tiff(path)
 #with tempfile.TemporaryDirectory() as path:
 #    images_from_path = convert_from_path('D:\\REP\\projects\\drawing_text_recognition\\pdf\\001сб.dwg.pdf', output_folder='D:\\REP\\projects\\drawing_text_recognition\\main', 
 #                                         poppler_path="D:\\REP\\projects\\drawing_text_recognition\\main\\poppler-24.02.0\\Library\\bin")    
+
+end = datetime.datetime.now()
+
+print(end-start)
---
+++ tests.py
@@ -0,0 +1,3 @@
+with open("11.prt", "r", encoding="utf-8") as f:
+    data = f.read()
+print(data)
